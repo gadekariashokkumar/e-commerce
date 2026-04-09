2fd4103cb7575c53e4defdf5a66d7618f49a85cc
ashokgadekari <gadekariashokkumar@gmail.com>
2026-04-09 10:31:13 +0530

    e-commerce

---
+++ E-commerce.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5" # replace with your AMI ID
+SG_ID="sg-062bf49da64d91a40" # replace with your SG ID
+ZONE_ID="Z0467851DLTIPRFDCIWG" # replace with your ID
+DOMAIN_NAME="gadekari.store" # replace with your domain name
+
+for instance in $@ # mongodb redis mysql
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.gadekari.store
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # gadekari.store
+    fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
